caa4b7fd7c0c4f9ab5a2e3b0811e1f4882b4c79b
eavelasquez <eavc.30@gmail.com>
2023-06-18 09:47:19 -0500

    feat: implement hooks

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ from reactpy.backend.fastapi import configure
 from fastapi import FastAPI
 
 @component
-def Item(text, done=False):
+def Item(text, initial_done=False):
+  done, set_done = hooks.use_state(initial_done)
   attrs = { "style": { "text-decoration": "line-through" } } if done else {}
-  return html.li(attrs, text)
+
+  def toggle_done(_):
+    set_done(not done)
+
+  if done:
+    return html.li(attrs, html.span(text))
+  else:
+    return html._(
+      html.li(
+        attrs,
+        html.span(text),
+        html.button({ "on_click": toggle_done }, "Done!")
+      ) 
+    )
+
 
 @component
 def HelloWorld():
+  attrs = { "style": { "text-decoration": "line-through" } }
+
   return html._(
     html.div(
       html.h1("Todo List"),
       html.ul(
-        { "style": { "list-style-type": "none" } },
+        attrs,
         Item("Learn ReactPy", True),
         Item("Learn FastAPI", True),
         Item("Build something awesome!", False)
