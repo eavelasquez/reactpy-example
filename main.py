78fdd30e1ef1a4985f9c44cbd4c132d798ecaba0
eavelasquez <eavc.30@gmail.com>
2023-06-18 09:08:59 -0500

    feat: update hello world component

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from fastapi import FastAPI
 
 @component
 def HelloWorld():
-  return html.h1("Hello, World!")
+  return html._(
+    html.div(
+      html.h1("Todo List"),
+      html.ul(
+        html.li("Learn React"),
+        html.li("Learn ReactPy"),
+        html.li("Build something awesome")
+      )
+    )
+  )
 
 app = FastAPI()
 configure(app, HelloWorld)
